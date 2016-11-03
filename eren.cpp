6b19c27f5f77f11ba48efb3eeb74e5fc5394c2a5
Spin12 <c.kilic@protonmail.ch>
2016-11-03 17:16:45 +0100

    Bugs debugged

--- eren.cpp
+++ eren.cpp
@@ -12,20 +12,23 @@ using namespace std;
 using namespace boost;
 
 
-void getCMO(vector< vector<string> >&, unsigned int&, string&, vector<float>&, ofstream&);
+void getCMO(vector< vector<string> >&, unsigned int&, string&, vector<double>&, ofstream&);
 void findBlock(vector< vector<string> >&, string&, unsigned int&, unsigned int&);
-void writeData(vector<float>&, vector<float>&, vector<float>&, vector<float>&,
-               vector<string>&, unsigned int&, vector<float>&, string&, ofstream&); 
+void writeData(vector<double>&, vector<double>&, vector<double>&, vector<double>&,
+               vector<string>&, unsigned int&, vector<double>&, string&, ofstream&); 
 
 
 int main(int argc, char* argv[]) {
 
 
   string kw ="CMO";
-  vector<float> ndaysSum={0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+  vector<double> ndaysSum={0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
 
   // Define output file name ----
   ofstream fileOut("output.csv");
+  // Write header
+  fileOut << "Rechnungen;Erstellung;;Validation I;;;Validation II;;;Validation III;;;Validation IV\n";
+  fileOut << ";Datum;User;Datum;User;Delta;Datum;User;Delta;Datum;User;Delta;Datum;User;Delta\n";
 
   // Read data ------------------
   string fileName = argv[1];
@@ -71,7 +74,7 @@ int main(int argc, char* argv[]) {
 
   nn0 = nn;
 
-  cout << " Progress:  " << fixed << setprecision(2) << float(nn0-nn)/float(nn0)*100.0 << "%";
+  cout << " Progress:  " << fixed << setprecision(2) << double(nn0-nn)/double(nn0)*100.0 << "%";
 
   string varID;
   unsigned int nPos;                  // Position of varID
@@ -87,12 +90,19 @@ int main(int argc, char* argv[]) {
   
   
     //varID="CMO - 2016 - 100696 - 0";
+    //varID="CMO - 2016 - 101759 - 0";
+    //varID="CMO - 2016 - 101702 - 0";
     getCMO(dataIn, nn, varID, ndaysSum, fileOut);
-  
-    if(float(nn0-nn)/float(nn0)*100.0<10) {
-      cout << "\b\b\b\b\b" <<  fixed << setprecision(2) << float(nn0-nn)/float(nn0)*100.0 << "%";
+
+/*
+  fileOut.close();
+  cout << endl << endl;
+  return 0;
+*/  
+    if(double(nn0-nn)/double(nn0)*100.0<10) {
+      cout << "\b\b\b\b\b" <<  fixed << setprecision(2) << double(nn0-nn)/double(nn0)*100.0 << "%";
     } else {
-      cout << "\b\b\b\b\b\b" <<  fixed << setprecision(2) << float(nn0-nn)/float(nn0)*100.0 << "%";
+      cout << "\b\b\b\b\b\b" <<  fixed << setprecision(2) << double(nn0-nn)/double(nn0)*100.0 << "%";
     }
 
   }
@@ -105,12 +115,12 @@ int main(int argc, char* argv[]) {
 
 
 
-inline void getCMO(vector< vector<string> >& dataIn, unsigned int& nn, string& varID, vector<float>& ndaysSum,  ofstream& fileOut) {
+inline void getCMO(vector< vector<string> >& dataIn, unsigned int& nn, string& varID, vector<double>& ndaysSum,  ofstream& fileOut) {
 
   unsigned int kk = 0, erstellen = 0, jj = 1, nn1 = 0, mm1 = 0;
-  float year0, month0, day0, hour0, min0, sec0, time0;
+  double year0, month0, day0, hour0, min0, sec0, time0;
   string user0;
-  vector<float> year_k(20,0), month_k(20,0), day_k(20,0), time_k(20,0);
+  vector<double> year_k(20,0), month_k(20,0), day_k(20,0), time_k(20,0);
   vector<string> user_k(20,"NaN");
   string sep1 = "", sep2 = "", sep3 = "";
   escaped_list_separator<char> sep;
@@ -123,6 +133,7 @@ inline void getCMO(vector< vector<string> >& dataIn, unsigned int& nn, string& v
       break;
     }
     nn--;
+    //cout << nn1 << "\t" << mm1 << endl;
   
     // Get date ------------------------------------------------------------
     {
@@ -179,6 +190,11 @@ inline void getCMO(vector< vector<string> >& dataIn, unsigned int& nn, string& v
         month_k[kk] = month0;
         day_k[kk] = day0;
         user_k[kk] = user0;
+/*
+        cout << endl << sec0 << "\t" << min0 << "\t" << hour0 << "\t" << day0 << "\t"
+                     << ndaysSum[int(month0)] << "\t" << year0 << "\t" << kk << "\t" <<  time_k[kk]
+                     << "\t" <<  time0;
+*/
       }
   
       for(int ll = 0; ll < dataIn[0].size();ll++){
@@ -210,7 +226,7 @@ inline void findBlock(vector< vector<string> >& dataIn, string& varID, unsigned 
       if((dataIn[mm1][0] == "") && (dataIn[mm1][1] == "") && (dataIn[mm1][2] == "") && 
          (dataIn[mm1][3] == "") && (dataIn[mm1][4] == "") && (dataIn[mm1][5] == "") && 
          (dataIn[mm1][6] == "") && (dataIn[mm1][7] == "") && (dataIn[mm1][8] == "") && 
-         (dataIn[mm1][2] == "") && (dataIn[mm1][10] == "")) {
+         (dataIn[mm1][9] == "") && (dataIn[mm1][10] == "")) {
         break;
       }
       mm1++;
@@ -219,24 +235,23 @@ inline void findBlock(vector< vector<string> >& dataIn, string& varID, unsigned 
 
 }
 
-inline void writeData(vector<float>& year_k, vector<float>& month_k, vector<float>& day_k,
-                      vector<float>& time_k, vector<string>& user_k, unsigned int& kk,
-                      vector<float>& ndaysSum, string& varID, ofstream& fileOut) {
+inline void writeData(vector<double>& year_k, vector<double>& month_k, vector<double>& day_k,
+                      vector<double>& time_k, vector<string>& user_k, unsigned int& kk,
+                      vector<double>& ndaysSum, string& varID, ofstream& fileOut) {
   
-  //ofstream fileOut("output.csv");
 
   if(kk==0) {
     time_t theTime = time(NULL);
     struct tm *aTime = localtime(&theTime);
   
-    float secNow = aTime->tm_sec;
-    float minNow = aTime->tm_min;
-    float hourNow = aTime->tm_hour;
-    float dayNow = aTime->tm_mday;
+    //double secNow = aTime->tm_sec;
+    //double minNow = aTime->tm_min;
+    //double hourNow = aTime->tm_hour;
+    double dayNow = aTime->tm_mday;
     int   monthNow = aTime->tm_mon + 1;    // Month is 0 - 11, add 1 to get a jan-dec 1-12 concept
-    float yearNow = aTime->tm_year + 1900; // Year is # years since 1900 
+    double yearNow = aTime->tm_year + 1900; // Year is # years since 1900 
 
-    float dtNow = yearNow*365+ndaysSum[monthNow]+dayNow
+    double dtNow = yearNow*365+ndaysSum[monthNow]+dayNow
                  -year_k[0]*365-ndaysSum[int(month_k[0])]-day_k[0];
 
     fileOut << varID << ";";
@@ -247,7 +262,7 @@ inline void writeData(vector<float>& year_k, vector<float>& month_k, vector<floa
   }
 
   if(kk==1) {
-    float dt = year_k[1]*365+ndaysSum[int(month_k[1])]+day_k[1]
+    double dt = year_k[1]*365+ndaysSum[int(month_k[1])]+day_k[1]
               -year_k[0]*365-ndaysSum[int(month_k[0])]-day_k[0];
 
    fileOut << varID << ";"; 
@@ -257,9 +272,9 @@ inline void writeData(vector<float>& year_k, vector<float>& month_k, vector<floa
 
 
   if(kk>1) {
-    float timeMin=2016050413355900;
+    double timeMin=2016050413355900;
     int itMin = -1;
-    vector<float> yearSor(20,0), monthSor(20,0), daySor(20,0);
+    vector<double> yearSor(20,0), monthSor(20,0), daySor(20,0);
     vector<string> userSor(20,"NaN");
 
     for(int ii = 1;ii <= kk; ii++) {
@@ -283,10 +298,10 @@ inline void writeData(vector<float>& year_k, vector<float>& month_k, vector<floa
    fileOut << day_k[0] << "." << month_k[0] << "." << year_k[0] << ";" << user_k[0];
 
    //int jj = 0;
-   vector<float> dt(20,0);
+   vector<double> dt(20,0);
    for(int ii=1; ii<=kk; ii++) {
      if(ii==1) {
-       dt[ii]= yearSor[1]*365 + ndaysSum[int(monthSor[1])] + daySor[1]-
+       dt[ii]= yearSor[1]*365 + ndaysSum[int(monthSor[1])] + daySor[1]
               -year_k[0]*365 - ndaysSum[int(month_k[0])] - day_k[0];
      } else {    
        //jj = ii-1;
